cb8f5e0d67784fccf99d7bb930272f6fb8617204
Dubravko <woose@web.de>
2019-03-05 16:10:49 +0100

    just a commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row"></div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"],
+  data: function() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10
+    };
+  }
+};
+</script>
+
+<style>
+.panel-body {
+  font-family: "Arizona", cursive;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+
+.quote {
+  cursor: pointer;
+}
+
+.quote:hover {
+}
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row"></div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"],
+  data: function() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10
+    };
+  }
+};
+</script>
+
+<style>
+</style>
